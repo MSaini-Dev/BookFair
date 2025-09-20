c9f5042df2c21f7566d60f8bc04ba45b994af3e2
Mohit Saini <m.saini.dev@gmail.com>
2025-09-20 13:03:34 +0530

    Update Messages.tsx

--- BookFair-Version-0.2/src/pages/Messages.tsx
+++ BookFair-Version-0.2/src/pages/Messages.tsx
@@ -1,4 +1,4 @@
-// import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { supabase } from "../services/supabase";
 import { useNavigate } from "react-router-dom";
 import { Button } from '@/components/ui/button';
